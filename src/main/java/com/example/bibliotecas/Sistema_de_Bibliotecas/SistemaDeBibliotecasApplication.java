326a758a97c4fdc86f98029105c06eff611dc054
Fabin-27 <fabiolourenco063@gmail.com>
2025-02-23 14:57:37 -0300

    Primeiro Commit

---
+++ src/main/java/com/example/bibliotecas/Sistema_de_Bibliotecas/SistemaDeBibliotecasApplication.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecas.Sistema_de_Bibliotecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeBibliotecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeBibliotecasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bibliotecas/Sistema_de_Bibliotecas/SistemaDeBibliotecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bibliotecas.Sistema_de_Bibliotecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeBibliotecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
