da3be3b37c5dc954ab2b68f9afd2c03e9219a8b6
Fabin-27 <fabiolourenco063@gmail.com>
2025-02-23 15:07:18 -0300

    Entidade Cliente e seus atributos criada com sucesso

---
+++ src/main/java/com/example/bibliotecas/Sistema_de_Bibliotecas/model/StatusDaConta.java
@@ -0,0 +1,7 @@
+package com.example.bibliotecas.Sistema_de_Bibliotecas.model;
+
+public enum StatusDaConta {
+
+    Ativo, Suspenso
+
+}
---
+++ src/main/java/com/example/bibliotecas/Sistema_de_Bibliotecas/model/TipoDeCliente.java
@@ -0,0 +1,7 @@
+package com.example.bibliotecas.Sistema_de_Bibliotecas.model;
+
+public enum TipoDeCliente {
+
+    Estudante, Professor, Visitante
+
+}
